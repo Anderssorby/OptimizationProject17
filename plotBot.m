0c1686bcffb679b8982f298c4b1aea8b56a878f3
siliusmv <siliusmv@stud.ntnu.no>
2017-02-20 20:41:29 +0100

    Update plotBot.m

--- plotBot.m
+++ plotBot.m
@@ -6,14 +6,14 @@ cords = zeros(2,length(l)+1);
 figure; hold on;
 
 for i = 1:length(l)
-    cords(:,i+1)=transpose(bigF(l,theta,i));
+    cords(:,i+1)=transpose(bigEff(l,theta,i));
 end
 
 for i = 1:length(l)
     plot([cords(1,i), cords(1,i+1)],[cords(2,i), cords(2,i+1)],'bl')
-    plot(cords(1,i),cords(2,i),'k*')
+    plot(cords(1,i),cords(2,i),'ko')
 end
-plot(cords(1,end),cords(2,end),'k*')
+plot(cords(1,end),cords(2,end),'k')
 grid on
 xlabel('x'), ylabel('y')
 title('Robot arm')
