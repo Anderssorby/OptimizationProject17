dc6972f427159fe06a9389e90038ec48e8b822fb
augustsm <jeglikerkakeee@hotmail.com>
2017-02-23 13:35:24 +0100

    Update gradf.m

--- gradf.m
+++ gradf.m
@@ -5,7 +5,7 @@ function res = gradf(l,theta,p)
     n = length(l); %Dimension of problem
     res = zeros(n,1); %Preallocating
     for i = 1:n
-        s1 = 0; 
+        s1 = 0; %Initialising the parameters needed for calculation of nabla(f)
         s2 = 0;
         k1 = 0;
         k2 = 0;
@@ -21,6 +21,6 @@ function res = gradf(l,theta,p)
             s1 = s1+l(j)*cos(a);
             s2 = s2+l(j)*sin(a);
         end
-        res(i) = -2*(k1-p(1))*s2+2*(k2-p(2))*s1;
+        res(i) = -2*(k1-p(1))*s2+2*(k2-p(2))*s1; %Calculate nabla(f) of index i
     end
 end
