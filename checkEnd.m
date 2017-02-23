307a0d5e761e694f81e2725756bd432d9be249d9
siliusmv <siliusmv@stud.ntnu.no>
2017-02-23 13:38:57 +0100

    Update checkEnd.m

--- checkEnd.m
+++ checkEnd.m
@@ -1,9 +1,16 @@
-function [bool,theta] = checkEnd(l,theta,p)
-e = f(l,theta,p);
-epsi = 1E-3;
+function [bool,theta] = checkEnd(l,theta,p) %Checks if end is a min point
+%bool = 1 if endpoint is a min-point
+%theta = angles between joints
+%l = vector of lengths of joints
+%p = point we try to reach
+
+e = f(l,theta,p); % value of f at endpoint
+epsi = 1E-3; %Choose some epsilon>0
+
+%Test values of f for close values of theta
 for i = 1:length(l)
     theta(i) = theta(i)+epsi;
-    if f(l,theta,p)<e
+    if f(l,theta,p)<e %This means we are not in a min point
         bool=0;
         return
     end
@@ -14,5 +21,5 @@ for i = 1:length(l)
     end
     theta(i)=theta(i)+epsi;
 end
-bool = 1;
+bool = 1; %All close values were larger than e. We are in a min point
 end
