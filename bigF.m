84295453ff5fb954143a88ae1400c9902e72248e
siliusmv <siliusmv@stud.ntnu.no>
2017-02-23 13:42:41 +0100

    Update bigF.m

--- bigF.m
+++ bigF.m
@@ -1,4 +1,7 @@
-function res = bigF(l,theta)
+function res = bigF(l,theta) %Returns the position of the robot arm for given lengths and angles
+%res = position of the arm (F(theta))
+%l = vector with lengths of joints
+%theta = vector with angles between joints
     s1 = 0;
     s2 = 0;
     for i = 1:length(l)
