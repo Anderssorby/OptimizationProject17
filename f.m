7ac6ca598a8062fbe0f0efc557d163aacfb34e3c
augustsm <jeglikerkakeee@hotmail.com>
2017-02-23 12:56:28 +0100

    Update f.m

--- f.m
+++ f.m
@@ -5,8 +5,9 @@ function res = f(l,theta,p)
     s1 = 0;
     s2 = 0;
     for i = 1:length(l)
-        s1 = s1+l(i)*cos(sum(theta(1:i))); %1st coordinate of the vector F-p
-        s2 = s2+l(i)*sin(sum(theta(1:i))); %2nd coordinate of the vector F-p
+        a = sum(theta(1:i)); %sum of angles up to index i
+        s1 = s1+l(i)*cos(a); %1st coordinate of the vector F
+        s2 = s2+l(i)*sin(a); %2nd coordinate of the vector F
     end
-    res = (s1-p(1))^2+(s2-p(2))^2; %Calculate the norm of F-p
+    res = (s1-p(1))^2+(s2-p(2))^2; %Calculate the squared 2-norm of F-p
 end
