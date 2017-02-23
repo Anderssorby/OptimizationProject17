f0f9f1103c790c04695ef00f75ff52408d1e0857
siliusmv <siliusmv@stud.ntnu.no>
2017-02-23 13:44:02 +0100

    Update bigEff.m

--- bigEff.m
+++ bigEff.m
@@ -1,4 +1,7 @@
-function res = bigEff(l,theta,n)
+function res = bigEff(l,theta,n) %Returns the position of the nth joint of the robot arm for given lengths and angles
+%res = position of the nth joint of the robot arm 
+%l = vector with lengths of joints
+%theta = vector with angles between joints
     s1 = 0;
     s2 = 0;
     for i = 1:min(n,length(l))
