b93aad07fc34fb08e792f4d9be4f808691a2f15b
augustsm <jeglikerkakeee@hotmail.com>
2017-02-23 13:07:09 +0100

    Update gradf.m

--- gradf.m
+++ gradf.m
@@ -1,14 +1,17 @@
 function res = gradf(l,theta,p)
-    n = length(l);
-    res = zeros(n,1);
+%l: the arm lengths
+%theta: the current angles
+%p: point to aproximate
+    n = length(l); %Dimension of problem
+    res = zeros(n,1); %Preallocating
     for i = 1:n
-        s1 = 0;
+        s1 = 0; 
         s2 = 0;
         k1 = 0;
         k2 = 0;
         for j = 1:i-1
-            a = sum(theta(1:j));
-            k1 = k1+l(j)*cos(a);
+            a = sum(theta(1:j)); %Find sum of angles up to index j
+            k1 = k1+l(j)*cos(a); 
             k2 = k2+l(j)*sin(a); 
         end
         for j = i:n
@@ -19,4 +22,5 @@ function res = gradf(l,theta,p)
             s2 = s2+l(j)*sin(a);
         end
         res(i) = -2*(k1-p(1))*s2+2*(k2-p(2))*s1;
+    end
 end
