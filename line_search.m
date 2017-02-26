3d3116a41101b9633165641c84f0b355fe8a7d34
augustsm <jeglikerkakeee@hotmail.com>
2017-02-26 18:02:04 +0100

    Update line_search.m

--- line_search.m
+++ line_search.m
@@ -1,8 +1,4 @@
 function [alpha] = line_search(phi, phiBar)
-% Algorithm 3.5 
-
-% alpha0
-%amax = 1;
 
 c1 = 1E-4;
 c2 = 0.1;
@@ -33,7 +29,6 @@ while i < 10
     if phia > phi0 + c1*alpha*phiBar0 || (phia > prevphi && i > 1)
         % alpha does not satisfy Armijo rule and we can use 
         % it as an upper bound in zoom
-        %disp('Line search: start zoom')
         alpha = zoom(phi, phiBar, constants, prevalpha, alpha);
         break
     end
@@ -42,13 +37,11 @@ while i < 10
     
     if abs(phiBara) <= -c2*phiBar0
         % Second Wolfe condition satisfied
-        %disp('Line search: Conditions satisfied')
         break
     end
 
     if phiBara >= 0
         % We are overstepping
-        %disp('Line search: Case #2')
         alpha = zoom(phi, phiBar, constants, alpha, prevalpha);
         break
     end
