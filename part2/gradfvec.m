cdf249e5eb696faaf973f225580eb4800ab6abbe
siliusmv <siliusmv@stud.ntnu.no>
2017-04-27 12:56:33 +0200

    Add files via upload

---
+++ part2/gradfvec.m
@@ -0,0 +1,11 @@
+function res = gradfvec(l,theta,p,pos,s)
+%l: the arm lengths
+%theta: the current angles
+%p: point to aproximate
+    a=cumsum(theta);
+    s1=cumsum(l.*cos(a),'reverse');
+    s2=cumsum(l.*sin(a),'reverse');
+    res=-(s1(1)-p(1))*s2+(s2(1)-p(2))*s1;
+    n = length(res);
+    res = [zeros((pos-1)*n;res;zeros((s-pos)*n];
+end
