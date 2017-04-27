915ce27eea25a0d03d4a4c0f5561658d44ce4245
siliusmv <siliusmv@stud.ntnu.no>
2017-04-27 14:52:59 +0200

    Add files via upload

--- part2/checkConstraints.m
+++ part2/checkConstraints.m
@@ -1,8 +1,8 @@
-function bool = checkConstraints(theta,pmat,s,ctol)
+function bool = checkConstraints(l,theta,pmat,n,s,ctol)
 bool = false;
 su = 0;
 for i = 1:s
-    su = su + f(l,theta(s*(i-1)+1:s*i),pmat(:,i));
+    su = su + f(l,theta(n*(i-1)+1:n*i),pmat(:,i));
 end
 if su < ctol
     bool = true;
