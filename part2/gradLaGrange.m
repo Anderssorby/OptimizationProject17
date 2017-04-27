4f1ca0cd6bba26f7f512eb06ae907bcee3f72c48
siliusmv <siliusmv@stud.ntnu.no>
2017-04-27 10:07:55 +0200

    Add files via upload

--- part2/gradLaGrange.m
+++ part2/gradLaGrange.m
@@ -3,7 +3,7 @@ function res = gradLaGrange(thetavec,lambda,mu,n,s,pmat,l)
 res = gradE(thetavec,n,s);
 
 for i = 1:s
-    res = res + (mu/2*f(l,thetavec(n*(i-1)+1:n*i)) - lambda(i))*gradf(l,thetavec(n*(i-1)+1:n*i));
+    res = res + (mu/2*f(l,thetavec(n*(i-1)+1:n*i),pmat(i,:)) - lambda(i))*gradf(l,thetavec(n*(i-1)+1:n*i),pmat(i,:));
 end
 
 end
