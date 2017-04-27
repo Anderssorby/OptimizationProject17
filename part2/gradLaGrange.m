acc3b463037119fd1372ae0d8dc5eb50707b0261
siliusmv <siliusmv@stud.ntnu.no>
2017-04-27 10:05:32 +0200

    Add files via upload

---
+++ part2/gradLaGrange.m
@@ -0,0 +1,9 @@
+function res = gradLaGrange(thetavec,lambda,mu,n,s,pmat,l)
+
+res = gradE(thetavec,n,s);
+
+for i = 1:s
+    res = res + (mu/2*f(l,thetavec(n*(i-1)+1:n*i)) - lambda(i))*gradf(l,thetavec(n*(i-1)+1:n*i));
+end
+
+end
---
+++ part2/laGrange.m
@@ -0,0 +1,10 @@
+function L = laGrange(thetavec,lambda,mu,n,s,pmat,l)
+
+L = E(thetavec,n,s);
+
+for i = 1:s
+    L = L - lambda(i)*f(l,thetavec(n*(i-1)+1:n*i),pmat(i,:)) + mu/2*(f(l,thetavec(n*(i-1)+1:n*i)))^2;
+end
+
+end
+    
