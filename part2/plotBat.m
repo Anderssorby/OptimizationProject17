e84be3175d30af5525a77cba67e531ed8921ca8e
siliusmv <siliusmv@stud.ntnu.no>
2017-04-27 15:23:07 +0200

    Add files via upload

---
+++ part2/plotBat.m
@@ -0,0 +1,28 @@
+function plotBat(l,theta,pmat)
+
+cords = zeros(2,length(l)+1);
+
+
+figure; hold on;
+for j = 1:length(pmat(1,:))
+    
+    for i = 1:length(l)
+        cords(:,i+1)=transpose(bigEff(l,theta((j-1)*length(l)+1:j*length(l)),i));
+    end
+
+    for i = 1:length(l)
+        plot([cords(1,i), cords(1,i+1)],[cords(2,i), cords(2,i+1)],'bl')
+        plot(cords(1,i),cords(2,i),'ko')
+    end
+    plot(cords(1,end),cords(2,end),'k')
+    %grid on
+end
+    
+    
+
+xlabel('x'), ylabel('y')
+title('Robot arm')
+for i = 1:length(pmat(1,:))
+    plot(pmat(1,i),pmat(2,i),'r*')
+end
+    
