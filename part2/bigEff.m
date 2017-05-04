3d77228f4e1db77e1ddbd199d6c3b3452109d588
siliusmv <siliusmv@stud.ntnu.no>
2017-05-04 12:28:06 +0200

    Add files via upload

---
+++ part2/bigEff.m
@@ -0,0 +1,9 @@
+function res = bigEff(l,theta,n)
+    s1 = 0;
+    s2 = 0;
+    for i = 1:min(n,length(l))
+        s1 = s1+l(i)*cos(sum(theta(1:i)));
+        s2 = s2+l(i)*sin(sum(theta(1:i)));
+    end
+    res=[s1;s2];
+end
