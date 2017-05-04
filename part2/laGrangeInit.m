cdd8e84edad04a84c0d93b99adf37d1eea085b6f
siliusmv <siliusmv@stud.ntnu.no>
2017-05-04 12:23:55 +0200

    Add files via upload

---
+++ part2/laGrangeInit.m
@@ -0,0 +1,10 @@
+function L = laGrangeInit(thetavec,lambda,mu,n,s,pmat,l)
+
+L = 0;
+
+for i = 1:s
+    L = L - lambda(2*i-1:2*i)'*(bigEff(l,thetavec(n*(i-1)+1:n*i),n)-pmat(:,i)) + mu*(f(l,thetavec(n*(i-1)+1:n*i),pmat(:,i)));
+end
+
+end
+    
