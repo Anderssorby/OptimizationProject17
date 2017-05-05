ac0c50865876e3674e26644bf5e0b99a493e5b49
siliusmv <siliusmv@stud.ntnu.no>
2017-05-05 13:47:41 +0200

    Add files via upload

--- part2/boundConstrainedLagrangian.m
+++ part2/boundConstrainedLagrangian.m
@@ -1,20 +1,35 @@
-function [theta,eval] = boundConstrainedLagrangian(l,pmat,theta,lambda,mu2,maxAngle,mu,tolOmega,tolEtha)
+function [theta,eval] = boundConstrainedLagrangian(l,p,theta,lambda,mu2,maxAngle,mu,tolOmega,tolEta)
+%l = nx1-dimensional vector with lengths of arm segments
+%p = 2xn-dimensional matrix with coordinates for points you want to reach
+%theta = s*nx1-dimensional vector with values for all angles
+%lambda = 2*sx1-dimensional vector with initial values for lambda
+%mu2 = penalty parameter for finding initial values
+%maxAngle = max allowed angle for the algorithm
+%mu = penalty parameter the gradient projection method
+%tolOmega = tolerance for the gradient in the gradient projection method
+%tolEta = tolerance for the norm of the C-vec in the gradient projection
+%method
+
+%eval = Value of E(theta) for final value of theta
+
+
+
 
 %Find initial values for theta
-theta = initVals(l,pmat,theta,maxAngle,lambda,mu2); 
+theta = initVals(l,p,theta,maxAngle,lambda,mu2); 
 
 %Choose parameters for line search
 c1=0.1;
 c2=0.9;
 
 %Define s and n
-s = length(pmat(1,:));
+s = length(p(1,:));
 n = length(theta)/s;
 
 
 % start params
 omega = 1/mu;
-etha = 1/(mu^0.1);
+eta = 1/(mu^0.1);
 
 %Projection function
 P = @(theta) boxProjection(theta, -maxAngle, maxAngle);
@@ -25,8 +40,8 @@ gd = @(theta,mu,lambda,omega,func,gradFunc) gradientBoxProjection(theta,omega,P,
 k = 0;
 while 1
     %Define augmented lagrangian and its gradient
-    func = @(theta) laGrange(theta,lambda,mu,n,s,pmat,l);
-    gradFunc = @(theta) gradLaGrange(theta,lambda,mu,n,s,pmat,l);
+    func = @(theta) laGrange(theta,lambda,mu,n,s,p,l);
+    gradFunc = @(theta) gradLaGrange(theta,lambda,mu,n,s,p,l);
     
     % minimise with given tolerances
     theta = gd(theta, mu, lambda,omega,func,gradFunc);
@@ -34,25 +49,25 @@ while 1
     %Make C-vector
     cvec = zeros(s,1);
     for i = 1:s
-        cvec(2*i-1:2*i) = bigEff(l,theta((i-1)*n+1:i*n),n)-pmat(:,i);
+        cvec(2*i-1:2*i) = bigEff(l,theta((i-1)*n+1:i*n),n)-p(:,i);
     end
 
-    if norm(cvec) <= etha
+    if norm(cvec) <= eta
         % test convergence
 
-        if norm(cvec) <= tolEtha && norm(gradFunc(theta)) <= tolOmega
+        if norm(cvec) <= tolEta && norm(gradFunc(theta)) <= tolOmega
             % Acceptable solution found
             break;
         end
         % Update multipliers tighten tolerances
         lambda = lambda - mu*cvec;
         %mu = mu;
-        etha = etha/(mu^0.9);
+        eta = eta/(mu^0.9);
         omega = omega/mu;
     else
         % Increase penalty parameter, tighten tolerances
         mu = 2*mu;
-        etha = 1/(mu^0.1);
+        eta = 1/(mu^0.1);
         omega = 1/mu;
     end
 
--- part2/equalityConstraintsLagrangian.m
+++ part2/equalityConstraintsLagrangian.m
@@ -1,4 +1,13 @@
-function [theta,eval] = equalityConstraintsLagrangian(l,pmat,theta,lambda,mu,tol)
+function [theta,eval] = equalityConstraintsLagrangian(l,p,theta,lambda,mu,tol)
+%l = nx1-dimensional vector with lengths of arm segments
+%p = 2xn-dimensional matrix with coordinates for points you want to reach
+%theta = s*nx1-dimensional vector with values for all angles
+%lambda = 2*sx1-dimensional vector with initial values for lambda
+%mu = penalty parameter
+%tol = tolerance for iterations with the BFGS-method
+
+%eval = Value of E(theta) for final value of theta
+
 
 %Tolerance for C-vec
 ctol = 1E-5;
@@ -7,11 +16,11 @@ ctol = 1E-5;
 c1=0.1;
 c2=0.9;
 
-if ~checkAnnulus(l,pmat)
+if ~checkAnnulus(l,p)
     error('Not reachable');
 end
 
-s = length(pmat(1,:));
+s = length(p(1,:));
 n = length(theta)/s;
 
 %Slacking/tightening factors for mu and tau
@@ -22,8 +31,8 @@ k=1;
 
 while 1
     %Define augmented Lagrangian and its gradient
-    func = @(theta) laGrange(theta,lambda,mu,n,s,pmat,l);
-    gradFunc = @(theta) gradLaGrange(theta,lambda,mu,n,s,pmat,l);
+    func = @(theta) laGrange(theta,lambda,mu,n,s,p,l);
+    gradFunc = @(theta) gradLaGrange(theta,lambda,mu,n,s,p,l);
     
     %Minimise the augmented lagrangian
     theta = BFGS(theta,tol,func,gradFunc,c1,c2);
@@ -31,7 +40,7 @@ while 1
     %Calculat C-vec
     cvec = zeros(2*s,1);
     for i = 1:s
-        cvec(2*i-1:2*i) = bigEff(l,theta(n*(i-1)+1:n*i),n)-pmat(:,i);
+        cvec(2*i-1:2*i) = bigEff(l,theta(n*(i-1)+1:n*i),n)-p(:,i);
     end
     
     %Check for convergence
