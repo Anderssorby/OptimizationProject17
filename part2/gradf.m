25d8e2ecd4c54c8852cb5f7803fb6535a06f75dc
siliusmv <siliusmv@stud.ntnu.no>
2017-05-05 15:21:39 +0200

    Delete gradf.m

--- part2/gradf.m
+++
@@ -1,9 +0,0 @@
-function res = gradf(l,theta,p)
-%l: the arm lengths
-%theta: the current angles
-%p: point to aproximate
-    a=cumsum(theta);
-    s1=cumsum(l.*cos(a),'reverse');
-    s2=cumsum(l.*sin(a),'reverse');
-    res=-(s1(1)-p(1))*s2+(s2(1)-p(2))*s1;
-end
